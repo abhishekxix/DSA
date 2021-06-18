ada59611be831ac7fe45012c3e37f79b4e21e76b
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-06-18 14:48:40 +0530

    add randomized quicksort

--- quicksort.cc
+++ quicksort.cc
@@ -21,6 +21,20 @@ int partition(vector<int> &a, int l, int r) {
   return i;
 }
 
+int randomized_partition(vector<int> &a, int l, int r) {
+  int i = (random() % (r - l + 1)) + l;
+  swap(a.at(i), a.at(r));
+  return partition(a, l, r);
+}
+
+void randomized_quicksort(vector<int> &a, int l, int r) {
+  if (l < r) {
+    int pivot = randomized_partition(a, l, r);
+    randomized_quicksort(a, l, pivot - 1);
+    randomized_quicksort(a, pivot + 1, r);
+  }
+}
+
 void quicksort(vector<int> &a, int l, int r) {
   if (l < r) {
     int pivot = partition(a, l, r);
@@ -30,8 +44,9 @@ void quicksort(vector<int> &a, int l, int r) {
 }
 
 int main() {
+  srand(time(nullptr));
   vector<int> a{5, 4, 3, 2, 1};
-  quicksort(a, 0, a.size() - 1);
+  randomized_quicksort(a, 0, a.size() - 1);
 
   return 0;
 }
